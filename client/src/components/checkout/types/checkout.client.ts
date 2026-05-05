f7788efe5b5a650fe1f63511377f3208017b8a38
vanthaita <thaitv225@gmail.com>
2026-05-05 14:49:50 +0700

    model(components-checkout): define checkout.client model

---
+++ client/src/components/checkout/types/checkout.client.ts
@@ -0,0 +1,20 @@
+export type BuyerState = {
+  fullName: string;
+  email: string;
+  phone: string;
+  city: string;
+  district: string;
+  ward: string;
+  street: string;
+  notes: string;
+  shipToDifferent: boolean;
+};
+
+export type AltState = {
+  altFullName: string;
+  altPhone: string;
+  altCity: string;
+  altDistrict: string;
+  altWard: string;
+  altStreet: string;
+};
